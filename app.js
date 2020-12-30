accdaae943e520e386a87732a7ad34cb8526cb53
Clems-web <clement.servais1@gmail.com>
2020-12-30 14:13:12 +0100

    pas fini

---
+++ app.js
@@ -0,0 +1,15 @@
+class modal = {
+    constructor(nom, prenom, age, passion) {
+        this.non = nom;
+        this.prenom = prenom;
+        this.age = age;
+        this.passion = passion;
+    }
+}
+
+let buttonModal = document.querySelector("#modal");
+
+buttonModal.addEventListener("click", ()=> {
+    let creaDiv = document.createElement("div");
+
+})
