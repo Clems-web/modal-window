35cfcc29793c17b75279da037202cc08730f8673
Clems-web <clement.servais1@gmail.com>
2020-12-31 14:07:51 +0100

    pas fini

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-class modal = {
-    constructor(nom, prenom, age, passion) {
-        this.non = nom;
-        this.prenom = prenom;
-        this.age = age;
-        this.passion = passion;
+class modal  {
+    constructor(texte) {
+        this.text = texte;
+    }
+    close() {
+        
     }
 }
 
-let buttonModal = document.querySelector("#modal");
+let buttonModal = document.getElementById("modal");
 
 buttonModal.addEventListener("click", ()=> {
     let creaDiv = document.createElement("div");
+    creaDiv.id = "popup";
+    creaDiv.style.width = "100%";
+    creaDiv.style.height = "100vh";
+    creaDiv.style.background = "dimgrey";
+    creaDiv.style.opacity = "0.5";
+    creaDiv.style.position = "absolute";
+    creaDiv.style.zindex = "10";
+    creaDiv.style.left = "0";
+    creaDiv.style.top = "0";
 
+
+
+
+    document.body.append(creaDiv);
 })
