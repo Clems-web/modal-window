ec7ce02f78678ef76973324d5b9f202a43ba05fc
Clems-web <clement.servais1@gmail.com>
2021-01-02 17:43:02 +0100

    Finito !

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
-class modal  {
-    constructor(texte) {
+class Modal  {
+    constructor(titre, texte) {
+        this.titre = titre;
         this.text = texte;
+
     }
+    init() {
+        let creaDiv = document.createElement("div");
+        creaDiv.id = "popup";
+        document.body.append(creaDiv);
+
+        let creaContent = document.createElement("div");
+        creaContent.id = "modal-window";
+        creaDiv.append(creaContent);
+
+        let divTitre = document.createElement("div");
+        divTitre.id = "divTitre";
+        divTitre.innerHTML = this.titre;
+        creaContent.append(divTitre);
+
+        let divTexte = document.createElement("div");
+        divTexte.id = "divTexte";
+        divTexte.innerHTML = this.text;
+        creaContent.append(divTexte);
+
+        let divButton = document.createElement("div");
+        divButton.id = "divButton";
+        creaContent.append(divButton);
+
+        let buttonAccept = document.createElement("button");
+        buttonAccept.id = "buttonAccept";
+        buttonAccept.innerHTML = "Accept";
+
+        let buttonDecline = document.createElement("button");
+        buttonDecline.id = "buttonDecline";
+        buttonDecline.innerHTML = "Decline";
+
+        divButton.append(buttonAccept);
+        divButton.append(buttonDecline);
+    };
+
     close() {
-        
+        document.body.lastChild.remove();
     }
 }
 
 let buttonModal = document.getElementById("modal");
 
 buttonModal.addEventListener("click", ()=> {
-    let creaDiv = document.createElement("div");
-    creaDiv.id = "popup";
-    creaDiv.style.width = "100%";
-    creaDiv.style.height = "100vh";
-    creaDiv.style.background = "dimgrey";
-    creaDiv.style.opacity = "0.5";
-    creaDiv.style.position = "absolute";
-    creaDiv.style.zindex = "10";
-    creaDiv.style.left = "0";
-    creaDiv.style.top = "0";
+    let blabla = new Modal("Fenêtre modal", "Acceptez-vous que nous enregistrions des données sur cet appareil ?");
+    blabla.init();
 
-
-
-
-    document.body.append(creaDiv);
+    buttonAccept.addEventListener("click", function () {
+        blabla.close();
+    })
+    buttonDecline.addEventListener("click", function () {
+        blabla.close();
+    })
 })
+
